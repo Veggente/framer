4aa917c5825fad02b2bfc224e4a7e96dc683ec91
Veggente <veggente@gmail.com>
2013-11-06 10:50:31 -0700

    Second commit

---
+++ framer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  framer
+//
+//  Created by Veggente on 11/5/13.
+//  Copyright (c) 2013 Xiaohan Kang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
